18e015276da9ebb35a793c65752b7f4fdee800e2
Nelu_17 <chauhannilay888@gmail.com>
2026-04-17 21:00:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,4 @@ from streamlit_gsheets import GSheetsConnection
 st.info("Something coming soon with a very cool concept. Stay tuned! 😉")
 conn = st.connection("gsheets", type=GSheetsConnection)
 existing = conn.read(worksheet="Sheet1", ttl=0)
+st.dataframe(existing)
