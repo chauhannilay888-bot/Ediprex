e1348b7d3e4be57982dda80abb00ad5845632ca7
Nelu_17 <chauhannilay888@gmail.com>
2026-04-17 20:51:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit as st
 st.info("Something coming soon with a very cool concept. Stay tuned! 😉")
+conn = st.connection("gsheets", type="gspread")
+
+# Read "Sheet1" into a DataFrame
+file_path = conn.read(worksheet="Sheet1")
+
+# Display in Streamlit
+st.dataframe(file_path)
